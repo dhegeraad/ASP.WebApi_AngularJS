336385e28758039aa7788fa340f84939e4dad06b
Roy Gerritse <rgerritse@e-id.nl>
2015-06-14 15:54:53 +0200

    Model

---
+++ Angular_MVC/Models/ManageInfoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Angular_MVC.Models
+{
+    public class ManageInfoViewModel
+    {
+        public string LocalLoginProvider { get; set; }
+
+        public string Email { get; set; }
+
+        public IEnumerable<UserLoginInfoViewModel> Logins { get; set; }
+
+        public IEnumerable<ExternalLoginViewModel> ExternalLoginProviders { get; set; }
+    }
+}
---
+++ Angular_MVC/Models/UserLoginInfoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Angular_MVC.Models
+{
+    public class UserLoginInfoViewModel
+    {
+        public string LoginProvider { get; set; }
+
+        public string ProviderKey { get; set; }
+    }
+}
